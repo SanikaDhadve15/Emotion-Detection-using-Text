990ed4d8b9073d6e2a9a8976ba54062055efeb44
Sanika Dhadve <140679118+SanikaDhadve15@users.noreply.github.com>
2024-10-06 16:05:03 +0530

    Add files via upload

---
+++ project_details.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def show_project_details():
+    st.title("EMOTION DETECTION USING TEXT.")
+    st.markdown(""" 
+    ### Emotion Detection System
+    The Emotion Detection Using Text application is a user-friendly web-based tool designed to analyze and identify emotions expressed in text inputs. Utilizing advanced natural language processing techniques, this application can detect various emotions from both English and Marathi text, providing users with immediate visualization.
+
+    **Group Member Names:** SIDDHI AVHAD, SANIKA DHADVE, PURVAJA GANGURDE
+    """)
